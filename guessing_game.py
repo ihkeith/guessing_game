b9a369f7c01467865e20a926222562d6ec4c3e9f
Ian Keith <ian@ianhkeith.com>
2019-04-22 12:04:45 -0500

    Removed pseudocode and unneeded comments.

--- guessing_game.py
+++ guessing_game.py
@@ -1,14 +1,3 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
-
 # Information on list sorting was found here: https://docs.python.org/3.3/howto/sorting.html
 
 import random, time
@@ -29,24 +18,6 @@ def display_welcome():
 
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
-
     display_welcome()
 
     secret_number = random.randint(1, 10)
@@ -55,7 +26,6 @@ def start_game():
     high_score = []
     play_a_game = input("Would you like to play? Yes/no  ")
 
-   # while player_guess != secret_number:
     while play_a_game.lower() == 'yes':
         player_guess = input("Please pick a number >  ")
         guesses += 1
@@ -95,5 +65,4 @@ def start_game():
 
 
 if __name__ == '__main__':
-    # Kick off the program by calling the start_game function
     start_game()
